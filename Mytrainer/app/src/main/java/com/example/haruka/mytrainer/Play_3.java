c748c006d6c8fc068291eab67b289ce013886baa
haruka1009 <haruka@Hirose-no-MacBook-Air.local>
2016-12-03 17:05:48 +0900

    button

--- Mytrainer/app/src/main/java/com/example/haruka/mytrainer/Play_3.java
+++ Mytrainer/app/src/main/java/com/example/haruka/mytrainer/Play_3.java
@@ -1,7 +1,10 @@
 package com.example.haruka.mytrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Play_3 extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Play_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_3);
+
+        Button sendButton = (Button) findViewById(R.id.tofin);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),osusume_2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Mytrainer/app/src/main/java/com/example/haruka/mytrainer/osusume_2.java
+++ Mytrainer/app/src/main/java/com/example/haruka/mytrainer/osusume_2.java
@@ -1,11 +1,13 @@
 package com.example.haruka.mytrainer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class osusume_2 extends AppCompatActivity {
 
@@ -16,12 +18,21 @@ public class osusume_2 extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button sendButton = (Button) findViewById(R.id.to_Play);
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),Play_3.class);
+                startActivity(intent);
+            }
+        });
+
+        Button sendButton2 = (Button) findViewById(R.id.tofin2);
+        sendButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),card_5.class);
+                startActivity(intent);
             }
         });
     }
